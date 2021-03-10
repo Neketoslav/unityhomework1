84a11287b39f90d1d09cc9ec472bfb8e9b88fc67
Nikita <stalker6661666@gmail.com>
2021-03-10 10:39:30 +0300

    lesson2

---
+++ Assets/Scripts/PlayerHealth.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerHealth : MonoBehaviour
+{
+    // Start is called before the first frame update
+    [SerializeField] private int _health;
+
+    public void HurtPlayer(int damage)
+    {
+        print("Ouch:" + damage);
+
+        _health -= damage; ;
+
+        if (_health <= 0)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
